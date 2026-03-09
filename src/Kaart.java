26abac063adce0690f1f3767705d7c3a987edc4f
clonnix <noahbrouwers24@gmail.com>
2026-03-09 14:46:45 +0100

    added non functional code

--- src/Kaart.java
+++ src/Kaart.java
@@ -1,7 +1,30 @@
 public class Kaart
 {
+    private double saldo;
+    private String kaartnummer;
+    private boolean geldig;
+    private boolean isIngecheckt;
+
     public static void main(String[] args)
     {
 
-        }
     }
+
+    public void inchecken(double instapTarief)
+    {
+
+    }
+
+    public double getSaldo()
+    {
+        return saldo;
+    }
+
+    public boolean getGeldig()
+    {
+        return geldig;
+    }
+    public boolean getIsIngecheckt(){
+        return isIngecheckt;
+    }
+}
--- src/Paal.java
+++ src/Paal.java
@@ -1,2 +1,25 @@
-public class Paal {
+public class Paal
+{
+    private double instapTarief = 10;
+
+
+    public void inchecken(Kaart Kaart)
+    {
+        if (Kaart.getGeldig() == false)
+        {
+            System.out.println("Error: Uw kaart is niet geldig");
+            return;
+        }
+        if (Kaart.getIsIngecheckt() == true){
+            System.out.println("Error: Uw kaart is al ingechecked");
+            return;
+        }
+    }
+
+
+
+    public double getInstapTarief()
+    {
+        return instapTarief;
+    }
 }
